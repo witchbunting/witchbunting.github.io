926e23139170a185bfc98f0d82bdf669ececd1d8
witchbunting <845986940@qq.com>
2024-12-09 10:26:28 +0800

    Site updated: 2024-12-09 10:26:14

--- photo/handle.py
+++ photo/handle.py
@@ -10,7 +10,7 @@ def rename_images_in_directory(directory_path):
     # 遍历并重命名每个文件
     for index, filename in enumerate(files, start=1):
         old_path = os.path.join(directory_path, filename)
-        new_filename = f"{index}.jpg"  # 按序号生成新的文件名
+        new_filename = f"s{index}.jpg"  # 按序号生成新的文件名
         new_path = os.path.join(directory_path, new_filename)
         
         # 重命名文件
@@ -18,4 +18,4 @@ def rename_images_in_directory(directory_path):
         print(f"Renamed '{filename}' to '{new_filename}'")
 
 # 示例调用
-rename_images_in_directory("观鸟/plant_and_other/")
+rename_images_in_directory("birding\\bird")
